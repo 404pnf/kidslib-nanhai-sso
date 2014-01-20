fe830c7507f4a47465e103e60b33479eaf1fabbf
404pnf <404pnf@exmaple.org>
2014-01-20 14:39:56 +0800

    增加rakefile和注释

--- server.rb
+++ server.rb
@@ -1,5 +1,16 @@
+# ## 依赖
 require 'sinatra'
+# require 'rexml' # because it's in the std lib
+require 'http' # https://github.com/tarcieri/http
 
+# ----
+# ## 如何使用
+#
+#      ruby server.rb
+#
+# 站点会运行在 http://localhost:4567
+
+# ## 整体思路说明
 # 1. 第一次验证通过后，我们需要把验证信息对应用户在本地存一下，并设定过期时间
 # 2. 这个信息用cookie的形式让用户也存着
 # 3. 如果过期了，我们要求用户再去验证，如果没有过期.就继续允许用户访问
@@ -8,39 +19,55 @@ require 'sinatra'
 #    只需确保ticket没有过期。
 # 5. 每次用户访问一个页面，在本地的db中延长一下ticket过期时间
 
+# ----
+# ## sinatra全局设定
+# URI.encode_www_form([['AppId', 'kidslib'], ['server', 'http://0.0.0.0:4567']])
 configure do
-  enable :sessions
+  enable :sessions # all request will have session either we set it or rack:session sets it automatically
   set :site_url, 'http://0.0.0.0/'
   set :session_valid_for, 60 * 1 # 60 minutes
-  set :sso_server, 'http://sso.server.ip'
+  set :sso_server, 'http://218.245.2.174:8080'
 end
 
+# ----
+# ## 帮助函数
 helpers do
 
   # 单线程跑这个程序，内存中就会持久化这个DB
   # 之前测试不行是因为每次请求 shotgun 都重新载入这个文件
   DB = {}
 
-  def logged_in?(ticket)
-    valid?(ticket) ? true : false
+  # ----
+  # interface呗。程序中直接调用只有interface这两个函数
+  def save_ticket(ticket)
+    DB[ticket] = Time.now.to_i
   end
 
   def valid?(ticket)
-    if ticket?(ticket) && !expired?(ticket)
-      extend_ticket_time ticket
-      true
-    else
-      delete_ticket ticket
-      false
-    end
+    !expired?(ticket) || (redirect '/login')
+  end
+
+  # ----
+  # helper functions
+  def expired?(ticket)
+    ticket?(ticket) && (Time.now.to_i - timestamp(ticket) > settings.session_valid_for)
   end
 
   def ticket?(ticket)
-    DB[ticket] # either return a timestamp or nil
+    DB[ticket] || remote_ticket?(ticket)
   end
 
-  def expired?(ticket)
-    Time.now.to_i - ticket?(ticket) > settings.session_valid_for
+  # ----
+  # 4 登陆后需要再次验证时候的接口:
+  # http://sso.server.ip.address/ssoServer/serviceValidate
+  # 登录到 ssoServer 成功后,返回到子系统,拿到 ticket 后,再拿 ticket 到上述接口地
+  # 址去请求一下,以得到对应的用户名。
+  def remote_ticket?(ticket)
+    HTTP.get 'http://218.245.2.174:8080/ssoServer/serviceValidate'
+  end
+
+  def timestamp(ticket)
+    DB[ticket].to_i # 如果 DB[ticket] 的值是 nil，会转为数字0，好让 expired? 函数做时间上的加减。
   end
 
   def extend_ticket_time(ticket)
@@ -51,53 +78,72 @@ helpers do
     DB.delete ticket
   end
 
-  def save_ticket(ticket)
-    DB[ticket] = Time.now.to_i
-  end
-
 end
 
-before '/ebooklist/*' do
-  if valid?(session['ticket'])
+# ----
+# ## before filter
+# 会运行在所有请求前
+# 静态文件除外！！
+# 这是sinatra源码中写死的。符合逻辑。可惜我正好不希望这样
+# 因此后面采取了些非常规手段 ；）
+before '/*.html' do
+  if valid? session['ticket']
     pass
   else
     redirect '/login'
   end
 end
 
+# ----
+# ## 路径 + HTTP VERB
 get '/' do
-  "#{DB.to_s}"
-  # redirect '/index.html'
+  redirect '/index.html'
 end
 
-# 1 直接登录 sso 的地址(假设未登录任何子系统,可以直接到本入口地址进行登录,登录后 再自动跳转回指定的子系统)
-# 接口地址:http://sso.server.ip.address/ssoServer/login 提交给该接口的参数列表(get 形式的参数与 post 方式均可):
-# 1)service:认证结束后跳转地址(希望登录成功后跳转到哪儿去的地址,例如 http://xxx/yyy.asp)
-# 2)AppId:子系统标识 ID(在 UCenter 中定义每个子系统的 AppId,也是 ssoServer 中的AppId)
-# 3)signData:数据签名(用 MD5 对 service+AppId + 固定的混淆码加密)(本签名非必须)
+# ----
+# ## 登陆
 get '/login' do
-  'ready to login?'
-  # redirect "http://sso.server.ip.address/ssoServer/login?AppId=kidslib;service:#{settings.site_url}/set;signData=digest_msg"
+  '据说登陆后学习效果更佳！'
+  redirect 'http://218.245.2.174:8080/ssoServer/login?AppId=kidslib&server=http%3A%2F%2F0.0.0.0%3A4567'
 end
 
+# ----
+# ## 登出
+# 3 退出sso需要调用的接口:
+# http://218.245.2.174:8080/nanhai/singleLogout
+# Sso退出,这个退出调用,是删除调CAS的ticket 各子系统的session需要各子系统自己删除。
 # 暂时没有提供用户logout的地方，也不准备提供。我们可以吧session过期时间设定短一点
+# FIXME: 如何通知sso删除这个ticket？
 get '/logout' do
+  delete_remote_ticket session['ticket']
   session.clear
   redirect '/'
 end
 
+# ----
+# ## 设置session
 # 本接口返回的 service 地址后面带的参数:
 # ticket(ticket 会在 service 地址后自动加上,
 # 例如:http://xxx/yyy.asp?Ticket=qweury03432432423ktjgj)
 get '/set-session' do
   ticket = params['Ticket']
-  session['ticket'] = ticket
-  save_ticket ticket
-  "#{ticket? ticket}"
-  # redirect to('/')
+  if remote_ticket?(ticket)
+    session['ticket'] = ticket
+    save_ticket ticket
+    redirect '/'
+  else
+    redirect '/login'
+  end
 end
 
-# html中放站点所有html文件，且要保持目录
+get '/db' do
+  "#{DB.to_s}"
+end
+
+# ----
+# ## 需要保护的html文件
+# 1. 将要保护的html文件 从 public 目录移到与 server.rb 平行的html目录
+# 1. 且要保持目录
 get '/*' do |path|
   begin
     File.read "html/#{path}"
